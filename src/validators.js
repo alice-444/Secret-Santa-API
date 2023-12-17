db9c8a76977d282f21e9a4eae5c131d297f23d04
Arisu Ryohei <carole.2002@outlook.com>
2023-12-17 22:56:33 +0100

    user and group model files & validators file

--- src/validators.js
+++ src/validators.js
@@ -1,13 +1,40 @@
 const yup = require("yup");
 
-const signSchema = yup.object({
+const loginSchema = yup.object().shape({
   email: yup.string().email().required(),
-  password: yup.string().min().required(),
+  password: yup.string().required(),
 });
 
-const updateSchema = yup.object({
-  email: yup.string().email(),
-  password: yup.string().min(6),
+const registerSchema = yup.object().shape({
+  username: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
 });
 
-module.exports = { signSchema, updateSchema };
+const updateUserSchema = yup.object().shape({
+  username: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});
+
+const createGroupSchema = yup.object().shape({
+  name: yup.string().required(),
+});
+
+const updateGroupSchema = yup.object().shape({
+  name: yup.string().required(),
+});
+
+const inviteToGroupSchema = yup.object().shape({
+  groupId: yup.string().required(),
+  emails: yup.array(yup.string().email()).min(1).required(),
+});
+
+module.exports = {
+  loginSchema,
+  registerSchema,
+  updateUserSchema,
+  createGroupSchema,
+  updateGroupSchema,
+  inviteToGroupSchema,
+};
